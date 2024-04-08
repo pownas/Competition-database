d16a87db711c94bdeee27adbdf1a058b5fa059a6
Jonas Arvidson <pownas@outlook.com>
2024-04-09 00:14:55 +0200

    Created new initial project without hyphen -

--- src/CompetitionDB.AppHost/Program.cs
+++ src/CompetitionDB.AppHost/Program.cs
@@ -1,5 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.CompetitionDB>("competitionDB");
+builder.AddProject<Projects.CompetitionDB>("competitiondb");
 
 builder.Build().Run();
