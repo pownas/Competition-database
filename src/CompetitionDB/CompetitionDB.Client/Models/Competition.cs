c34c82145a3410305a5b1ee0894fb7bf0315172d
Jonas Arvidson <pownas@outlook.com>
2024-04-09 01:31:45 +0200

    Added first layout

---
+++ src/CompetitionDB/CompetitionDB.Client/Models/Competition.cs
@@ -0,0 +1,11 @@
+﻿namespace CompetitionDB.Client.Models
+{
+    public class Competition
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+        public List<Contestant> Contestants { get; set; }
+    }
+}
---
+++ src/CompetitionDB/CompetitionDB.Client/Models/Contestant.cs
@@ -0,0 +1,11 @@
+﻿namespace CompetitionDB.Client.Models;
+
+public class Contestant
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Scored { get; set; }
+
+    public int CompetitionId { get; set; }
+    public Competition Competition { get; set; }
+}
